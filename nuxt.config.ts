de083c526232eade4e13a25a38a0b9719184b75e
Luis Miguel Gilbert <luis.gilbert@bairesdev.com>
2023-10-24 01:27:52 -0500

    use netlify preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    preset: 'netlify'
+  },
   experimental: { componentIslands: true },
   css: [
     //'@/assets/main.css',
