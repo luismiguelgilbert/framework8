a5d69757d66a40e3c5805e3560f58f2789b623c8
Luis Miguel Gilbert <luis.gilbert@bairesdev.com>
2024-01-14 18:23:37 -0500

    chore: update typing

--- typings/server/filter_payload.ts
+++ typings/server/filter_payload.ts
@@ -1,21 +1,26 @@
 import { z } from "zod"
 
 export enum filter_keys_enum {
+  DEFAULT = "isDefault",
+  STATUS = "status",
+  SORT = "sortBy",
+  SEARCH = "searchString",
   PAGE = "page",
   ROWS = "rowsPerPage",
-  SEARCH = "searchString",
-  SORT = "sortBy",
-  STATUS = "status",
   ID = "id",
 }
 
 export type filter_payload = Partial<Record<filter_keys_enum, string>>;
 
 export const filter_payload_object = z.object({
-  [filter_keys_enum.PAGE] : z.coerce.string().default('1'),
-  [filter_keys_enum.ROWS]: z.coerce.string().default('10'),
-  [filter_keys_enum.SEARCH]: z.coerce.string().default(''),
-  [filter_keys_enum.SORT]: z.coerce.string().default('1'),
+  //Required
   [filter_keys_enum.STATUS]: z.coerce.string().default('1'),
+  [filter_keys_enum.SORT]: z.coerce.string().default('1'),
+  [filter_keys_enum.SEARCH]: z.coerce.string().default(''),
+  //Optional
+  [filter_keys_enum.DEFAULT]: z.coerce.string().default('1'),
   [filter_keys_enum.ID]: z.coerce.string(),
+  //Temporary
+  [filter_keys_enum.PAGE] : z.coerce.string().default('1'),
+  [filter_keys_enum.ROWS]: z.coerce.string().default('20'),
 });
