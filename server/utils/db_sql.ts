220a79cc0f003574f9f1f5d08aecab6f69a06c09
Luis Miguel Gilbert <luis.gilbert@bairesdev.com>
2024-01-24 03:15:01 -0500

    fix: add tedious dependency

--- server/utils/db_sql.ts
+++ server/utils/db_sql.ts
@@ -1,8 +1,10 @@
 import { Sequelize } from 'sequelize';
+import * as tedious from 'tedious';
 
 const sequelize = new Sequelize(process.env.SQL_DATABASE_NAME!, process.env.SQL_DATABASE_USR!, process.env.SQL_DATABASE_PWD!, {
   host: process.env.SQL_DATABASE_URL!,
   dialect: 'mssql',
+  dialectModule: tedious,
   dialectOptions: {
     options: {
       // Your tedious options here
